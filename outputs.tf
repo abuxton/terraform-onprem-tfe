d8f015f70d563e3ebb797cfb3097046d38966bb5
Adam Buxton <buxton.adam@gmail.com>
2021-09-15 15:04:19 +0100

    add some structure and terraform-docs

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+variable "physical" {
+  type        = bool
+  default     = true
+  description = "Boolean regarding deployment on physical servers or using a virtualization provider where supported."
+}
