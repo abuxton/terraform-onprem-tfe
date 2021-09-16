aa0c7eed8d717ce150e64efbbf31eaabcbf0f9b8
Adam Buxton <buxton.adam@gmail.com>
2021-09-16 11:16:53 +0100

    update comments

--- tests/physical-demo/main.tf
+++ tests/physical-demo/main.tf
@@ -1,11 +1,11 @@
 module "tfe" {
   source = "../.."
-  #   tfe_license_filepath = "./test.rli"
+  // tfe_license_filepath = "./test.rli"
   tfe_hostname     = "localhost"
   enc_password     = "password"
   console_password = "password"
   physical         = true
-  # run `vagrant up` from the `terraform-onprem-tfe/tests/physical-demo` folder
+  // run `vagrant up` from the `terraform-onprem-tfe/tests/physical-demo` folder
   connection_private_key = ".vagrant/machines/default/virtualbox/private_key"
   connection_user        = "vagrant"
   connection_port        = 2222
